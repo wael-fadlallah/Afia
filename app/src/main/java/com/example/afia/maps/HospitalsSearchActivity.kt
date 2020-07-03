9bb70f5da228562d6f43cfcb0505851f2cfd1453
Your Name <you@example.com>
2020-07-03 10:10:53 +0200

    working with motion editor

--- app/src/main/java/com/example/afia/maps/HospitalsSearchActivity.kt
+++ app/src/main/java/com/example/afia/maps/HospitalsSearchActivity.kt
@@ -1,5 +1,6 @@
 package com.example.afia.maps
 
+import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
@@ -92,6 +93,16 @@ class HospitalsSearchActivity : AppCompatActivity(),
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
         mMap.isMyLocationEnabled = true
         mMap.setOnMyLocationButtonClickListener(this)
         mMap.setOnMyLocationClickListener(this)
